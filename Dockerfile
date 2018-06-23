fc1c602070b5bdddc727fdde3c8e98d90707334a
Tran Thanh Tung <thanhtung100397@gmail.com>
2018-06-23 20:37:07 +0700

    Add env for database in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,21 @@
-#FROM openjdk:8-jdk-alpine
-#VOLUME /tmp
-#ARG JAR_FILE
-#COPY ${JAR_FILE} app.jar
-#ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
+FROM openjdk:8-jdk-alpine
+VOLUME /tmp
+ARG JAR_FILE
+COPY ${JAR_FILE} app.jar
+ENV DB_URL 127.0.0.1:3306
+ENV DB_USERNAME root
+ENV DB_PASSWORD 1111
+ENTRYPOINT ["java", \
+            "-Djava.security.egd=file:/dev/./urandom", \
+            "-jar", \
+            "/app.jar", \
+            "--spring.datasource.url=jdbc:mysql://${DB_URL}/user_management_database?createDatabaseIfNotExist=true&useUnicode=yes&characterEncoding=UTF-8&autoReconnect=true&useSSL=true", \
+            "--spring.datasource.username=${DB_USERNAME}", \
+            "--spring.datasource.password=${DB_PASSWORD}"]
 
-FROM gradle:4.8.0-jdk8-alpine
-WORKDIR /
-COPY . app/
-USER root
-RUN gradle build --stacktrace
-USER gradle
-CMD java -jar app/build/libs/user-management-0.0.1-SNAPSHOT.jar
+#FROM gradle:4.8.0-jdk8-alpine
+#COPY . /app/
+#WORKDIR /app/
+#USER root
+#RUN gradle build --stacktrace
+#CMD java -jar /app/build/libs/user-management-0.0.1-SNAPSHOT.jar
--- src/main/java/com/service/usermanagement/UserManagementApplication.java
+++ src/main/java/com/service/usermanagement/UserManagementApplication.java
@@ -1,7 +1,7 @@
 package com.service.usermanagement;
 
-import com.rabbitmq.client.AMQP;
-import com.service.usermanagement.models.dto.MessageReceiver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
@@ -12,16 +12,18 @@ import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 //@EnableEurekaClient	`
 public class UserManagementApplication {
-
 //	@Bean
 //	@LoadBalanced
 //	public RestTemplate restTemplate(){
