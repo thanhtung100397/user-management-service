bcac2be1ef98b133a1c841a485902b7b58bee33e
Tran Thanh Tung <thanhtung100397@gmail.com>
2018-07-04 11:55:43 +0700

    remove Eureka service discovery client

--- src/main/java/com/service/usermanagement/UserManagementApplication.java
+++ src/main/java/com/service/usermanagement/UserManagementApplication.java
@@ -3,22 +3,12 @@ package com.service.usermanagement;
 import com.service.usermanagement.services.DataFakerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -27,13 +17,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Arrays;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class UserManagementApplication implements CommandLineRunner {
-//	@Bean
-//	@LoadBalanced
-//	public RestTemplate restTemplate(){
-//		return new RestTemplate();
-//	}
 
     public static void main(String[] args) {
         SpringApplication.run(UserManagementApplication.class, args);
