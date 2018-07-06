ff909ff9f8cff582ea2ddec36213441e115a4405
Tran Thanh Tung <thanhtung100397@gmail.com>
2018-07-06 11:20:37 +0700

    Add configuarion for Consul

--- src/main/java/com/service/usermanagement/UserManagementApplication.java
+++ src/main/java/com/service/usermanagement/UserManagementApplication.java
@@ -1,20 +1,11 @@
 package com.service.usermanagement;
 
 import com.service.usermanagement.services.DataFakerService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class UserManagementApplication implements CommandLineRunner {
